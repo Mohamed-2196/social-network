402eb83008f21e3d1e622569e08f6a523cbe4302
smahdija <shmhassan500@gmail.com>
2024-12-18 15:50:23 +0300

    database schema

---
+++ backend/db/migration/001_users_table_up.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "users" (
+    "user_id" varchar(255) not null,
+    "created_at" datetime not null default CURRENT_TIMESTAMP,
+    "email" varchar(255) not null,
+    "first_name" varchar(255) not null,
+    "last_name" varchar(255) not null,
+    "nickname" varchar(255) null,
+    "birthday" datetime not null,
+    "image" varchar(255) null,
+    "about" TEXT null,
+    "password" varchar(255) not null,
+    primary key ("user_id")
+);
