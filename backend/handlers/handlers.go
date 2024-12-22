8a30a4a1a2124dc9ac3796542d11e10faf9d7145
mohakhalil <mohamed.khalil2196@gmail.com>
2024-12-22 16:17:51 +0300

    profile semi complete

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -9,7 +9,8 @@ func AddHandlers(r *mux.Router) {
 	r.HandleFunc("/signup", SignUpHandler).Methods("POST")  // Specify POST method
 	r.HandleFunc("/signin", SignInHandler).Methods("POST")  // Specify POST method
 	r.HandleFunc("/logout", SignOutHandler).Methods("POST") // Specify POST method
-	r.HandleFunc("/cook", Validcookie).Methods("POST")       // Specify GET method
-	r.HandleFunc("/profile", UserDataHandler).Methods("POST")       // Specify GET method
+	r.HandleFunc("/cook", Validcookie).Methods("POST")       
+	r.HandleFunc("/profile", UserDataHandler).Methods("POST")       
+	r.HandleFunc("/profile/edit", EditProfileHandler).Methods("POST")       // Specify GET method
 
 }
