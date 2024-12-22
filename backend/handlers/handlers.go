2614365f5183622ad7bed6b1f7c138221f22a6a2
smahdija <shmhassan500@gmail.com>
2024-12-22 20:37:27 +0300

    db changed / handler changed / home.tsx changed

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -11,6 +11,6 @@ func AddHandlers(r *mux.Router) {
 	r.HandleFunc("/logout", SignOutHandler).Methods("POST") // Specify POST method
 	r.HandleFunc("/cook", Validcookie).Methods("POST")       // Specify GET method
 	r.HandleFunc("/profile", UserDataHandler).Methods("POST")
-	r.HandleFunc("/", CreatePostHandler).Methods("POST")
+	r.HandleFunc("/createpost", CreatePostHandler).Methods("POST")
 	
 }
