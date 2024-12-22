1143ccde45510ba16666c7d501344fe922032caf
reemohamed <workc237@gmail.com>
2024-12-22 15:49:00 +0300

    chat

---
+++ app/chat/page.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Nav from "../components/nav";
+import Chatpic from '../components/chatpic';
+import ChatBox from '../components/ChatBox';
+
+const page = () => {
+  return (
+    <>
+      <div>
+        <Nav/>
+      </div>
+
+      <div className="flex w-full h-screen">
+        {/* Left Section: 30% */}
+        <div className="w-[30%] p-4 bg-gray-800">
+          <div className='chatlist'>
+            <div className="card bg-base-200 shadow-md mb-4">
+              <div className="card-body p-4">
+                <h2 className="card-title text-lg font-bold">Messages</h2>
+              </div>
+            </div>
+
+            <ul className="menu menu-md bg-base-200 rounded-box w-[100%] h-200">
+              <li><a> <Chatpic /> lg item 1</a></li>
+              <li><a> <Chatpic /> lg item 1</a></li>
+              <li><a> <Chatpic /> lg item 1</a></li>
+              <li><a> <Chatpic /> lg item 1</a></li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Divider */}
+      
+
+        {/* Right Section: 70% */}
+        <div className="w-[70%] bg-base-200 p-4 flex flex-col h-full">
+          <div className="card bg-base-200 mb-4 border-2 border-gray-300 rounded-lg">
+            <div className="card-body p-2">
+              <h2 className="card-title text-lg font-bold">User</h2>
+            </div>
+          </div>
+
+          <div className="flex-1 overflow-auto">
+            <ChatBox />
+          </div>
+
+          {/* Typing Component */}
+          <div className="fixed bottom-0 right-0 w-[70%] p-4 bg-gray-800 text-white mt-auto">
+            <div className="flex items-center space-x-4">
+              <button className="btn btn-outline">Default</button>
+              <input
+                type="text"
+                className="w-full p-2 rounded bg-gray-700 text-white"
+                placeholder="Type something..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
